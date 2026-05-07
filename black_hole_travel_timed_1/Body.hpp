61d4b58bf4a773359336032979f44081be4a1899
dtchgondoit <amjadjamalshadi@gmail.com>
2026-05-07 06:27:58 -0400

    Improved Body physics system with acceleration, impulse handling, and simulation utilities

--- black_hole_travel_timed_1/Body.hpp
+++ black_hole_travel_timed_1/Body.hpp
@@ -1,7 +1,6 @@
-// mass velocity position, accelerations , radius and trail history
-#pragma once
 #pragma once
 #include "Vec3.hpp"
+#include <vector>
 
 class Body
 {
@@ -11,12 +10,28 @@ private:
 public:
     Vec3 position;
     Vec3 velocity;
+    Vec3 acceleration;
+
     float mass;
+    float inverseMass;
+
     float radius;
+
+    bool isStatic;
+
+    std::vector<Vec3> trailHistory;
 
     Body(float m = 1.0f, float r = 1.0f);
 
     void applyForce(const Vec3& force);
+
+    void applyImpulse(const Vec3& impulse);
+
     void integrate(float deltaTime);
+
     void clearForces();
+
+    float getSpeed() const;
+
+    float kineticEnergy() const;
 };
