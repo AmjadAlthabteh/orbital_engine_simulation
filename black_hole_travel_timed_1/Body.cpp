24181f6b19300572cc50e54750ada4ebd74d98c2
dtchgondoit <amjadjamalshadi@gmail.com>
2026-06-26 23:01:22 -0400

    Initialize body mass state

--- black_hole_travel_timed_1/Body.cpp
+++ black_hole_travel_timed_1/Body.cpp
@@ -3,22 +3,39 @@
 Body::Body(float m, float r)
 {
     mass = m;
+    inverseMass = mass > 0.0f ? 1.0f / mass : 0.0f;
     radius = r;
     position = Vec3(0, 0, 0);
     velocity = Vec3(0, 0, 0);
+    acceleration = Vec3(0, 0, 0);
     forceAccum = Vec3(0, 0, 0);
+    isStatic = mass <= 0.0f;
 }
 
 void Body::applyForce(const Vec3& force)
 {
+    if (isStatic) return;
+
     forceAccum = forceAccum + force;
+}
+
+void Body::applyImpulse(const Vec3& impulse)
+{
+    if (isStatic) return;
+
+    velocity = velocity + impulse * inverseMass;
 }
 
 void Body::integrate(float deltaTime)
 {
-    if (mass <= 0.0f) return;
+    if (isStatic || inverseMass <= 0.0f)
+    {
+        clearForces();
+        acceleration = Vec3(0, 0, 0);
+        return;
+    }
 
-    Vec3 acceleration = forceAccum * (1.0f / mass);
+    acceleration = forceAccum * inverseMass;
 
     velocity = velocity + acceleration * deltaTime;
     position = position + velocity * deltaTime;
@@ -29,4 +46,14 @@ void Body::integrate(float deltaTime)
 void Body::clearForces()
 {
     forceAccum = Vec3(0, 0, 0);
+}
+
+float Body::getSpeed() const
+{
+    return velocity.length();
+}
+
+float Body::kineticEnergy() const
+{
+    return 0.5f * mass * velocity.lengthSquared();
 }
